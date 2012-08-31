7cf5c877607809b24b07f070ee81ee345e90fee2
tianzhou <tianzhouchen@gmail.com>
2012-08-31 00:04:48 -0700

    move statement to a separate file

--- PandaSQL/Parser/ParserDriver.h
+++ PandaSQL/Parser/ParserDriver.h
@@ -3,14 +3,12 @@
 
 #include "PandaDB.h"
 
-#include "Catalog/Table.h"
+#include "Statement.h"
 
 #include "VFS/File.h"
 
 #include "Utils/Expr/BooleanExpr.h"
 #include "Utils/Expr/Expr.h"
-#include "Utils/Join.h"
-#include "Utils/Predicate.h"
 #include "Utils/Status.h"
 #include "Utils/Types.h"
 
@@ -20,90 +18,6 @@
 
 namespace PandaSQL
 {
-
-class Statement
-{
-public:
-	Statement(PandaDB *io_pDB);
-	~Statement();
-
-	enum StatementType
-	{
-		kStmtUnknown = 0,
-		kStmtSelect = 1,
-		kStmtUpdate = 2,
-		kStmtInsert = 3,
-		kStmtDelete = 4,
-		kStmtCreateTable = 5,
-		kStmtCreateIndex = 6,
-		kStmtDropTable = 7,
-		kStmtDropIndex = 8,
-	};
-
-	void Clear();
-
-	void SetOriginalStmtText(const std::string inStmtText);
-
-	void SetStatementType(StatementType inStmtType) { mStmtType = inStmtType; }
-	StatementType GetStatementType() const { return mStmtType; }
-
-	//For create_table_stmt, created table[1]
-	//For create_index_stmt, affected table[1]
-	//For drop_table_stmt, affected table[1]
-	//For update_stmt, affected table[1]
-	//For insert_stmt, affected table[1]
-	//For delete_stmt, affected table[1]
-	//For select_stmt, affected table[1..N]
-	void AddTableRef(const std::string &inTableRef);
-
-	//For update_stmt, applied value[1..N]
-	//For insert_stmt, applied value[1..N]
-	void AddExprRef(const Expr &inExpr);
-
-	//For create_index_stmt, indexed column[1]
-	//For select_stmt, selected column[1..N]
-	//For insert_stmt, affected column[1..N]
-	//For update_stmt, affected column[1..N]
-	void AddColumnDefWithName(const ColumnQualifiedName &inQualifiedName);
-
-	//For cerate_table_stmt, column def[1..N]
-	void AddColumnDef(const ColumnDef &inDef);
-	void AddAllColumns();
-
-	//For create_index_stmt
-	void SetIndexRef(const std::string &inIndexRef);
-
-	void SetPredicate(const Predicate &inPredicate);
-
-	//Rewrite statment. e.g. Translate to fully qualified column name
-	Status Prepare();
-
-	Status Execute(bool loadTable);
-	void PrintStatement();
-
-	void SetWhereClauseExpression(const BooleanExpr *inWhereExpr);
-
-private:
-	PandaDB *mpDB;
-
-	std::string	mOrigStmtText;
-
-	StatementType mStmtType;
-	Table::TableRefList mTableRefs;
-
-	ExprList mSetExprList;
-
-	ColumnDefList mColumnDefs;
-	bool	mAllColumns;
-
-	JoinList mJoinList;
-
-	Predicate mPredicate;
-
-	std::string mIndexRef;
-
-	const BooleanExpr *mpWhereExpr;
-};
 
 class ParserDriver
 {
---
+++ PandaSQL/Parser/Statement.cpp
@@ -0,0 +1,249 @@
+#include "stdafx.h"
+
+#include "Statement.h"
+
+#include "PandaDB.h"
+#include "Utils/Debug.h"
+
+#include <iostream>
+
+namespace PandaSQL
+{
+
+Statement::Statement(PandaDB *io_pDB)
+:
+mpDB(io_pDB)
+,mStmtType(kStmtUnknown)
+,mAllColumns(false)
+,mpWhereExpr(NULL)
+{
+}
+
+Statement::~Statement()
+{
+}
+
+void Statement::Clear()
+{
+	mOrigStmtText = std::string();
+	mStmtType = kStmtUnknown;
+	mTableRefs.clear();
+	mSetExprList.clear();
+	mColumnDefs.clear();
+	mIndexRef = std::string();
+}
+
+void Statement::SetOriginalStmtText(const std::string inStmtText)
+{
+	mOrigStmtText = inStmtText;
+
+	//Replace line feed with space
+
+	std::string space(" ");
+	std::string crlf("\r\n");
+	std::string lf("\n");
+
+	size_t pos = 0;
+	while((pos = mOrigStmtText.find(crlf, pos)) != std::string::npos)
+	{
+		mOrigStmtText.replace(pos, crlf.length(), space);
+		pos += space.length();
+	}
+
+	pos = 0;
+	while((pos = mOrigStmtText.find(lf, pos)) != std::string::npos)
+	{
+		mOrigStmtText.replace(pos, lf.length(), space);
+		pos += space.length();
+	}
+}
+
+void Statement::AddTableRef(const std::string &inTableRef)
+{
+	mTableRefs.push_back(inTableRef);
+}
+
+void Statement::AddExprRef(const Expr &inExpr)
+{
+	mSetExprList.push_back(inExpr);
+}
+
+void Statement::AddColumnDefWithName(const ColumnQualifiedName &inQualifiedName)
+{
+	ColumnDef theColumnDef;
+	theColumnDef.qualifiedName = inQualifiedName;
+	mColumnDefs.push_back(theColumnDef);
+}
+
+void Statement::AddColumnDef(const ColumnDef &inDef)
+{
+	mColumnDefs.push_back(inDef);
+}
+
+void Statement::AddAllColumns()
+{
+	mAllColumns = true;
+}
+
+void Statement::SetIndexRef(const std::string &inIndexRef)
+{
+	mIndexRef = inIndexRef;
+}
+
+void Statement::SetPredicate(const Predicate &inPredicate)
+{
+	mPredicate = inPredicate;
+}
+
+Status Statement::Prepare()
+{
+	Status result;
+
+	result = mPredicate.Prepare(*mpDB, mTableRefs);
+	mPredicate.TransformToCNF();
+
+	if (mAllColumns)
+	{
+		PDASSERT(mColumnDefs.empty());
+
+		Table::TableRefList::const_iterator iter = mTableRefs.begin();
+		Table *theTable;
+		for (; iter != mTableRefs.end(); iter++)
+		{
+			result = mpDB->GetTableByName(*iter, &theTable);
+
+			if (result.OK())
+			{
+				const ColumnDefList& theColumnDefList = theTable->GetAllColumns();
+				mColumnDefs.insert(mColumnDefs.end(), theColumnDefList.begin(), theColumnDefList.end());
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	else
+	{
+		if (mStmtType == kStmtCreateTable)
+		{
+			ColumnDefList::iterator iter = mColumnDefs.begin();
+			uint32_t columnIndex = 0;
+
+			for (; iter != mColumnDefs.end(); iter++)
+			{
+				iter->qualifiedName.tableName = mTableRefs[0];
+				iter->index = columnIndex;
+				columnIndex++;
+			}
+		}
+		else
+		{
+			ColumnDefList::iterator iter = mColumnDefs.begin();
+
+			for (; iter != mColumnDefs.end(); iter++)
+			{	
+				result = mpDB->AmendColumnDef(mTableRefs, &(*iter));				
+			}
+		}
+	}
+	
+	this->PrintStatement();
+
+	if (!result.OK())
+	{
+		std::cout << "Error:" << result.GetCode() << std::endl;
+	}
+
+	return result;
+}
+
+Status Statement::Execute(bool loadTable)
+{
+	Status result;
+
+	switch (mStmtType)
+	{
+	case kStmtCreateTable:
+		{
+			if (loadTable)
+			{
+				result = mpDB->OpenTable(mTableRefs[0]);
+			}
+			else
+			{
+				result = mpDB->CreateTable(mTableRefs[0], mColumnDefs);
+			}
+
+			break;
+		}
+	case kStmtInsert:
+		{
+			result = mpDB->InsertData(mTableRefs[0], mColumnDefs, mSetExprList);
+			break;
+		}
+	case kStmtSelect:
+		{
+			//ColumnQualifiedName join1 = {"Master", "master_id"};
+			//ColumnQualifiedName join2 = {"Detail", "detail_id"};
+			//mJoinList.push_back(join1);
+			//mJoinList.push_back(join2);
+
+			TuplePredicate tuplePredicate;
+			result = mpDB->SelectData(mTableRefs, mJoinList, mColumnDefs, NULL);
+			break;
+		}
+	case kStmtDelete:
+		{
+			//Predicate thePredicate;
+			//PredicateItem predicateItem;
+
+			//PandaSQL::Expr lExpr;
+			//lExpr.type = kExprColumnDef;
+
+			//ColumnQualifiedName qualifiedName = {"", "name"};
+			//ColumnDef theDef;
+			//theDef.qualifiedName = qualifiedName;
+			//theDef.index = kInvalidColumnIndex;
+			//theDef.dataType = kText;
+			//theDef.constraintType = kConstraintNone;
+			//
+			//lExpr.columnDef = theDef;
+
+			//PandaSQL::Expr rExpr;
+			//rExpr.type = kExprText;
+			//rExpr.text = "\"Peter\"";
+
+			//predicateItem.SetFormat(lExpr, rExpr, PredicateItem::kEqual);
+			//thePredicate.SetSinglePredicateItem(predicateItem);
+			//result = mpDB->DeleteData(mTableRefs[0], &thePredicate);
+			result = mpDB->DeleteData(mTableRefs[0], NULL);
+			break;
+		}
+
+	default:
+		break;
+	}
+
+	return result;
+}
+
+void Statement::PrintStatement()
+{
+	mPredicate.Print(0);
+
+	std::cout << "*********ColumnDef*********" << std::endl;
+
+	for (size_t i = 0; i < mColumnDefs.size(); i++)
+	{
+		std::cout << "Column " << i << ": " << mColumnDefs[i].qualifiedName.tableName << "." << mColumnDefs[i].qualifiedName.columnName << std::endl;
+	}
+}
+
+void Statement::SetWhereClauseExpression(const BooleanExpr *inWhereExpr)
+{
+	mpWhereExpr = inWhereExpr;
+}
+
+
+}	// PandaSQL
