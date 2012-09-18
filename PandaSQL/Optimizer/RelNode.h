67199ab7348de2528907036c933c97d3dfe95061
tianzhou <tianzhouchen@gmail.com>
2012-09-18 01:13:33 -0700

    add projection list, predicate list to RelNode; Also add kRelBase, kRelJoin type to RelNode

--- PandaSQL/Optimizer/RelNode.h
+++ PandaSQL/Optimizer/RelNode.h
@@ -10,10 +10,20 @@ namespace PandaSQL
 
 class Table;
 
+typedef std::vector<uint32_t> ProjectionList;
+typedef std::vector<uint32_t> PredicateList;
+
 class RelNode : public Node
 {
 
 public:
+
+	enum RelType
+	{
+		kRelUnknown = 0,
+		kRelBase = 1,
+		kRelJoin = 2
+	};
 
 	RelNode(const Table *inTable);
 	~RelNode();
@@ -21,10 +31,19 @@ public:
 	const Table* GetTable() const { return mpTable; }
 	ValueList* GetTupleValue() { return &mTupleValueList; }
 
+	const ProjectionList& GetProjectionList() const { return mProjectionList; }
+	void SetProjectionList(const ProjectionList &inProjectionList) { mProjectionList = inProjectionList; }
+
+	const PredicateList& GetPredicateList() const { return mPredicateList; }
+	void SetPredicateList(const PredicateList &inPredicateList) { mPredicateList = inPredicateList; }
+
 private:
 
 	const Table *mpTable;
 	ValueList mTupleValueList;
+
+	ProjectionList mProjectionList;
+	PredicateList mPredicateList;
 
 
 };
